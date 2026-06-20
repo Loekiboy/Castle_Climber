5920044777576c538a4a2aea8885687b1e8799de
Loekiboy <loekoerlemans@gmail.com>
2026-06-20 11:04:48 +0200

    Added grass tileset and added camera

--- player.gd
+++ player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -800.0
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta: float) -> void: 
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -22,3 +22,4 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.play("idle")
 
 	move_and_slide()
+ 
